9ed2a846c20b16afc7a4cca4782b8be683e8889a
PCWelling <paul@epicureandiving.com>
2017-11-28 14:35:17 -0800

    mid-day2

--- PythonStack/Django_Projects/ninjagold2/apps/ninjagold/views.py
+++ PythonStack/Django_Projects/ninjagold2/apps/ninjagold/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from datetime import datetime
+import random
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+def index(request):
+    try:
+        request.session['total']
+    except KeyError:
+        request.session['total'] = 0
+    return render(request, "ninjagold/index.html")
+
+def proccess(request):
+    
