2f529cd6a2b0f30e52eca62d48fdeb23e4d4b90a
PCWelling <paul@epicureandiving.com>
2018-02-05 23:11:58 -0800

    eodeod

--- netCore/intro_to_net/puzzles/Program.cs
+++ netCore/intro_to_net/puzzles/Program.cs
@@ -9,9 +9,21 @@ namespace puzzles
         public static void RandomArray(){
             int [] numArray = new int[10];
             Random rand = new Random();
-            for(int i = 0; i < numArray.Length; i++){
+            int max = numArray[0];
+            int min = numArray[0];
+            int sum = 0;
 
+            for(int i = 0; i < numArray.Length; i++){
+                numArray[i] = rand.Next(5,25);
+                if(numArray[i] < min){
+                    min = numArray[i];
+                }
+                if(numArray[i] > max){
+                    max = numArray[i];
+                }
+                sum = sum + numArray[i];              
             }
+            Console.WriteLine("min is: {0}, max is: {1}, sum is: {2}", min, max, sum);
         }
 
         //// Coin Flip ////
@@ -21,6 +33,7 @@ namespace puzzles
         ////---------------------------------------------------------------------------////
         static void Main(string[] args)
         {
+            RandomArray();
 
             ////-----------------------------------------------------------------------////
         }
